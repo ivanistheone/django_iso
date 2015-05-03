a9567018f7f3aaf26dfbf6122376483f12201b06
Ivan Savov <ivan.savov@gmail.com>
2015-05-02 20:06:21 -0400

    Forgot to render props.comment
    Wow on a usability note --- these error messages aren't super useful.
    
    gotcha1: it seems each `render` function must return one HTML tag.
    You can't return two divs for example:
    
        <div id='header'>
        <div id='footer'>
    
    you but you could return one div that contains both:
    
        <div id="thepage">
          <div id='header'>
          <div id='footer'>
        </div>
    
    I think this is becasue reacts annotates the DOM with a bunch of `data-reactid`s which couldn't be attached if there were two elements...
    
    This is the rendered version as your browser sees it:
    
        <span id="reactComponent-components__App">
          <div id="theapp" data-reactid=".1quk2uzkkjk" data-react-checksum="-742137341">
            <div id="thesearchbox" data-reactid=".1quk2uzkkjk.0">
                  <div class="search-component" data-reactid=".1quk2uzkkjk.0.0">
                    <input type="text" data-reactid=".1quk2uzkkjk.0.0.0">
                          <p data-reactid=".1quk2uzkkjk.0.0.1">
                            <span data-reactid=".1quk2uzkkjk.0.0.1.0">
                              <span data-reactid=".1quk2uzkkjk.0.0.1.0.0">You are searching for: </span>
                              <span data-reactid=".1quk2uzkkjk.0.0.1.0.1"></span>
                           </span>
                        </p>
                  </div>
            </div>
            <div id="thecomment" data-reactid=".1quk2uzkkjk.1">
                  <p data-reactid=".1quk2uzkkjk.1.0">
                  <span data-reactid=".1quk2uzkkjk.1.0.0">comment = </span>
                  <span data-reactid=".1quk2uzkkjk.1.0.1">This is state passed from py to js</span>
                  </p>
            </div>
          </div>
        </span>

--- djangosite/static/components/App.jsx
+++ djangosite/static/components/App.jsx
@@ -1,12 +1,25 @@
+/**
+ * @jsx React.DOM
+ */
+
+
 var React = require("react"),
     Search = require("./Search.jsx");
 
 var App = React.createClass({
   render() {
     return (
-      <Search />
+      <div id="theapp">
+        <div id="thesearchbox">
+          <Search />
+        </div>
+        <div id="thecomment">
+          <p>comment = {this.props.comment}</p>
+        </div>
+      </div>
     );
   }
 });
+
 
 module.exports = App;
